02a13671c99fc4244175ce8fd6cc0ba290381c9c
Matias Senger <m.senger@hotmail.com>
2022-02-02 22:00:28 +0100

    Reverted some changes made in the pylandau version back to the original Root version

--- landaupy/langauss.py
+++ landaupy/langauss.py
@@ -5,11 +5,11 @@ def gaussian_pdf(x, mu, sigma):
 	return np.exp(-1/2*((x-mu)/sigma)**2)/sigma/(2*np.pi)**.5
 
 def pdf_not_vectorized(x: float, mu: float, eta: float, sigma: float) -> float:
-	"""Non vectorized langaus PDF calculation. This function should be avoided, this is almost a copy-paste from the original Root code in https://root.cern.ch/doc/master/langaus_8C.html only for testing purposes."""
+	"""Non vectorized and rustic langaus PDF calculation. This function should be avoided, this is almost a copy-paste from the original Root code in https://root.cern.ch/doc/master/langaus_8C.html only for testing purposes."""
 	if any([not isinstance(arg, (int,float)) for arg in [x,mu,eta,sigma]]):
 		raise TypeError('All arguments must be float numbers.')
 	
-	mpshift = 0 # -0.22278298;     // Landau maximum location shift in original code is wrong, since the shift does not depend on mu only
+	mpshift = -0.22278298 # Landau maximum location shift in original code is wrong, since the shift does not depend on mu only
 
 	np = 100 # number of convolution steps
 	sc = 8 # convolution extends to +-sc Gaussian sigmas
@@ -21,7 +21,7 @@ def pdf_not_vectorized(x: float, mu: float, eta: float, sigma: float) -> float:
 		np = 100000
 
 	# MP shift correction
-	mpc = mu - mpshift # * eta;
+	mpc = mu - mpshift * eta
 
 	# Range of convolution integral
 	xlow = x - sc * sigma
