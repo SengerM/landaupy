7fba9629815fc03742d25b9589fb095c9e618025
Matias Senger <m.senger@hotmail.com>
2022-02-03 00:26:29 +0100

    Stuff for the packaging process

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ import setuptools
 
 setuptools.setup(
 	name = "landaupy",
-	version = "0",
+	version = "0.0",
 	author = "Matias Senger",
 	author_email = "m.senger@hotmail.com",
 	description = "A pure python implementation of the Landau distribution",
 	url = "https://github.com/landaupy",
+	download_url = "https://github.com/SengerM/landaupy/archive/refs/tags/v0.0.tar.gz",
 	packages = setuptools.find_packages(),
 	classifiers = [
 		"Programming Language :: Python :: 3",
