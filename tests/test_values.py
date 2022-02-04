69651a817e72f5e23927de9d7d46233db9880981
Matias Senger <m.senger@hotmail.com>
2022-02-04 23:15:37 +0100

    Implemented test for checking normalization of PDFs

--- tests/test_values.py
+++ tests/test_values.py
@@ -2,8 +2,9 @@ import unittest
 import numpy as np
 from landaupy import landau, langauss
 from math import isclose
+import scipy.integrate as integrate
 
-DEBUGGING_PLOTS = True
+DEBUGGING_PLOTS = False
 
 if DEBUGGING_PLOTS == True:
 	import plotly.graph_objects as go
@@ -15,6 +16,8 @@ def areclose(A, B, rel_tol=1e-09, abs_tol=0.0) -> bool:
 class TestLandauValues(unittest.TestCase):
 	"""Test if the functions are producing the correct values."""
 	def test_pdf(self):
+		"""Compares the `landaupy.pdf` implementation against "the original
+		implementation" from Root."""
 		x_mpv_to_test = [0,1,11,111,1111,1e-3,1e-11]
 		x_mpv_to_test += [-x for x in x_mpv_to_test]
 		xi_to_test = [1e-22,1e-3,1,11,111,1e5,1e22]
@@ -47,10 +50,30 @@ class TestLandauValues(unittest.TestCase):
 		
 		if DEBUGGING_PLOTS == True:
 			fig.show()
+	
+	def test_normalization(self):
+		"""Test that the PDF integrates to 1."""
+		x_mpv_to_test = [0,1]
+		x_mpv_to_test += [-x for x in x_mpv_to_test]
+		xi_to_test = [1e-9,1e-3,1,11,111,1e5,1e22]
+		for x_mpv in x_mpv_to_test:
+			for xi in xi_to_test:
+				with self.subTest(i={'x_mpv': x_mpv, 'xi': xi}):
+					integral, err = integrate.quad(lambda x: landau.pdf(x, x_mpv=x_mpv, xi=xi), x_mpv-5*xi, x_mpv+2222*xi)
+					self.assertTrue(
+						isclose(
+							a = integral,
+							b = 1,
+							abs_tol = 1e-3,
+						),
+						f'integral={integral}, error={err}, was expecting integral≈1.',
+					)
 
 class TestLangaussValues(unittest.TestCase):
 	"""Test if the functions are producing the correct values."""
 	def test_pdf(self):
+		"""Compares the `landaupy.pdf` implementation against "the original
+		implementation" from Root."""
 		x_mpv_to_test = [0]
 		xi_to_test = [.01,1,11]
 		sigma_to_test = xi_to_test
@@ -84,6 +107,25 @@ class TestLangaussValues(unittest.TestCase):
 		
 		if DEBUGGING_PLOTS == True:
 			fig.show()
+	
+	def test_normalization(self):
+		"""Test that the PDF integrates to 1."""
+		x_mpv_to_test = [0]
+		xi_to_test = [1e-9,1e-3,1,11,111,1e5,1e22]
+		sigma_to_test = xi_to_test
+		for x_mpv in x_mpv_to_test:
+			for xi in xi_to_test:
+				for sigma in sigma_to_test:
+					with self.subTest(i={'x_mpv': x_mpv, 'xi': xi, 'sigma': sigma}):
+						integral, err = integrate.quad(lambda x: langauss.pdf(x, landau_x_mpv=x_mpv, landau_xi=xi, gauss_sigma=sigma), x_mpv-5*(xi+sigma), x_mpv+2222*xi+5*sigma)
+						self.assertTrue(
+							isclose(
+								a = integral,
+								b = 1,
+								abs_tol = 1e-3,
+							),
+							f'integral={integral}, error={err}, was expecting integral≈1.',
+						)
 
 if __name__ == '__main__':
 	unittest.main()
