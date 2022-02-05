8d5aadb5fc353b162b5ad7ecdd42530afc6fb805
Matias Senger <m.senger@hotmail.com>
2022-02-05 13:29:22 +0100

    Implemented test for `langauss`

--- tests/test_non_valid_parameters_values.py
+++ tests/test_non_valid_parameters_values.py
@@ -1,8 +1,6 @@
 import unittest
 import numpy as np
 from landaupy import landau, langauss
-from math import isclose
-import scipy.integrate as integrate
 
 class TestLandauNonValidParamters(unittest.TestCase):
 	"""Test that the functions behave as expected when parameters take
@@ -35,5 +33,42 @@ class TestLandauNonValidParamters(unittest.TestCase):
 					else: # All elements in the output should be NaN
 						self.assertTrue(all(np.isnan(cdf_values)), '`landau.cdf` is not returning NaN when it should.')
 
+class TestLangaussNonValidParamters(unittest.TestCase):
+	"""Test that the functions behave as expected when parameters take
+	non valid values."""
+	def test_pdf(self):
+		x_mpv_to_test = [0]
+		xi_to_test = [-1,-1e-20,0,1e-20,1]
+		sigma_to_test = [-1,-1e-20,0,1e-20,1]
+		
+		for x_mpv in x_mpv_to_test:
+			for xi in xi_to_test:
+				for sigma in sigma_to_test:
+					with self.subTest(i={'x_mpv': x_mpv, 'xi': xi, 'sigma': sigma}):
+						x = np.linspace(x_mpv-22*(np.abs(xi)+np.abs(sigma)), x_mpv+222*np.abs(xi)+11*np.abs(sigma),9999)
+						pdf_values = langauss.pdf(x, x_mpv, xi, sigma)
+						should_we_expect_NaN = xi < 0 or sigma < 0 or xi==sigma==0
+						if not should_we_expect_NaN:
+							self.assertFalse(any(np.isnan(pdf_values)), '`langauss.pdf` is returning NaN when it should not.')
+						else: # All elements in the output should be NaN
+							self.assertTrue(all(np.isnan(pdf_values)), '`langauss.pdf` is not returning NaN when it should.')
+	
+	def test_cdf(self):
+		x_mpv_to_test = [0]
+		xi_to_test = [-1,-1e-20,0,1e-20,1]
+		sigma_to_test = [-1,-1e-20,0,1e-20,1]
+		
+		for x_mpv in x_mpv_to_test:
+			for xi in xi_to_test:
+				for sigma in sigma_to_test:
+					with self.subTest(i={'x_mpv': x_mpv, 'xi': xi, 'sigma': sigma}):
+						x = np.linspace(x_mpv-5*(np.abs(xi)+np.abs(sigma)), x_mpv+22*np.abs(xi)+5*np.abs(sigma),99)
+						cdf_values = langauss.cdf(x, x_mpv, xi, sigma)
+						should_we_expect_NaN = xi < 0 or sigma < 0 or xi==sigma==0
+						if not should_we_expect_NaN:
+							self.assertFalse(any(np.isnan(cdf_values)), '`langauss.cdf` is returning NaN when it should not.')
+						else: # All elements in the output should be NaN
+							self.assertTrue(all(np.isnan(cdf_values)), '`langauss.cdf` is not returning NaN when it should.')
+	
 if __name__ == '__main__':
 	unittest.main()
